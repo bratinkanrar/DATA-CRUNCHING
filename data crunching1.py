65097ad3b70263f8119137b556ce451688c653c6
BRATIN KANRAR <59428554+bratinkanrar@users.noreply.github.com>
2023-02-02 11:01:56 +0530

    Delete data crunching1.py

--- data crunching1.py
+++
@@ -1,9 +0,0 @@
-import os
-import pandas as pd
-
-master_df=pd.DataFrame()
-for file in os.listdir(os.getcwd()):
-	if file.endswith('.tsv'):
-		master_df = master_df.append(pd.read_csv(File))
-master_df.to_csv('output.tsv',index=False)
-
